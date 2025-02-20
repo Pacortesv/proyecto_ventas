cc0e0a4b76598f2c9a1138e20b106377f348a5ef
Pacortesv <pa.cortesvergara@gmail.com>
2025-02-20 09:06:33 -0300

    Update analisis_ventas.py

--- analisis_ventas.py
+++ analisis_ventas.py
@@ -4,21 +4,21 @@ import matplotlib.pyplot as plt
 # archivo CSV
 df = pd.read_csv('ventas_productos.csv')
 
-# Calculo precio total por producto
+# Precio total por producto
 df['Precio_Total'] = df['Cantidad'] * df['Precio']
 
-# Primeros registros
+# Primer registro
 print(df.head())
 
-# Grafico precio total por producto
+# Grafico
 plt.bar(df['Producto'], df['Precio_Total'])
 plt.xlabel('Producto')
 plt.ylabel('Precio Total')
 plt.title('Precio Total por Producto')
 
-# grafico como PNG
+# grafico PNG
 plt.savefig('grafico_precios.png')
 
-# Grafico
+# grafico
 plt.show()
 
